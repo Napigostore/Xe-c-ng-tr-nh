eab66dc97e150a584955cd4cdcd826fc77f85be9
NAPIGO <truongtumoc@gmail.com>
2026-06-30 17:41:07 +0700

    Add policies config

---
+++ app/config/policies.ts
@@ -0,0 +1,8 @@
+import { shipping } from "./shipping";
+
+export const policies = [
+  "Bảo hành uy tín 7 ngày theo thông tin hiển thị trên sản phẩm.",
+  "Hỗ trợ đổi trả nếu sản phẩm lỗi kỹ thuật, sai mẫu hoặc hư hỏng do vận chuyển.",
+  "Tư vấn chọn xe theo độ tuổi, ngân sách và sở thích của bé trước khi đặt.",
+  shipping.note
+];
