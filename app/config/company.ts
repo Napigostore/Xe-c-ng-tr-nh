beb20e4df1d2d6aaef23177b8540396b1bafdef9
NAPIGO <truongtumoc@gmail.com>
2026-06-30 16:28:43 +0700

    Create company.ts

---
+++ app/config/company.ts
@@ -0,0 +1,15 @@
+export const company = {
+  name: "Napigo Eco Home",
+
+  slogan: "Đồ chơi công trình nhập khẩu chính hãng",
+
+  website: "https://dochoinhapkhau.tienso.vn",
+
+  address: "",
+
+  email: "",
+
+  taxCode: "",
+
+  copyright: "© 2026 Napigo Eco Home",
+};
